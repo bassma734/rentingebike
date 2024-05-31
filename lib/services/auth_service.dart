a66ae556f80f7325e1e95c91d2d21d119ae976a0
zeineb <kallelzeineb16@gmail.com>
2024-06-01 01:18:29 +0200

    "hhh"

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:renting_app/change_notifiers/registration_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -23,7 +22,7 @@ class AuthService {
   }) async {
     try {
       await _auth
-          .createUserWithEmailAndPassword(
+        .createUserWithEmailAndPassword(
         email: email,
         password: password,
       )
@@ -73,21 +72,9 @@ class AuthService {
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
+
     // Once signed in, return the UserCredential
-    final userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
-
-    final user = userCredential.user;
-    if (user != null) {
-      final userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-      if (!userDoc.exists) {
-        await _firestoreService.addUser(
-          user.displayName ?? '',
-          user.email ?? '',
-        );
-      }}
-
-    return userCredential;
-
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   static Future<void> resetPassword({required String email}) =>
